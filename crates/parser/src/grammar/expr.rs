49b06edc05e6727b85a3026a8973361de9f6c790
Clay <clayw@clay66.dev>
2023-08-20 17:32:19 +0100

    Set up pratt parsing operator precedence

--- crates/parser/src/grammar/expr.rs
+++ crates/parser/src/grammar/expr.rs
@@ -1,5 +1,41 @@
 use super::*;
 
-pub(crate) fn expr(p: &mut Parser) {
+pub(crate) fn expr(p: &mut Parser) -> Option<CompletedMarker> {
+    expr_bp(p)
+}
+
+fn expr_bp(p: &mut Parser) -> Option<CompletedMarker> {
     todo!()
+}
+enum InfixOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Quot,
+    Pow,
+}
+
+enum PrefixOp {
+    Neg,
+}
+
+impl InfixOp {
+    fn bp(&self) -> (u8, u8) {
+        match self {
+            Self::Add | Self::Sub => (1, 2),
+            Self::Mul | Self::Div => (3, 4),
+            Self::Mod | Self::Quot => (5, 6),
+            Self::Pow => (9, 8),
+        }
+    }
+}
+
+impl PrefixOp {
+    fn bp(&self) -> ((), u8) {
+        match self {
+            PrefixOp::Neg => ((), 7),
+        }
+    }
 }
