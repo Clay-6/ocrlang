83a3aa0ea6f42eadbb9f3835da0e21a1d50341ad
Clay <clayw@clay66.dev>
2023-08-24 21:45:32 +0100

    Implement `Sink::finish`

--- crates/parser/src/sink.rs
+++ crates/parser/src/sink.rs
@@ -1,5 +1,8 @@
+use std::mem;
+
 use lexer::Token;
-use rowan::GreenNodeBuilder;
+use rowan::{GreenNodeBuilder, Language};
+use syntax::OcrLang;
 
 use crate::{event::Event, parser::ParseError, Parse};
 
@@ -23,6 +26,68 @@ impl<'t, 'input> Sink<'t, 'input> {
     }
 
     pub(crate) fn finish(mut self) -> Parse {
-        todo!()
+        for idx in 0..self.events.len() {
+            match mem::replace(&mut self.events[idx], Event::Placeholder) {
+                Event::StartNode {
+                    kind,
+                    forward_parent,
+                } => {
+                    let mut kinds = vec![kind];
+
+                    let mut idx = idx;
+                    let mut forward_parent = forward_parent;
+
+                    while let Some(fp) = forward_parent {
+                        idx += fp;
+
+                        // Traverse along the `forward_parent` chain until we can't anymore
+                        forward_parent = if let Event::StartNode {
+                            kind,
+                            forward_parent,
+                        } =
+                            mem::replace(&mut self.events[idx], Event::Placeholder)
+                        {
+                            kinds.push(kind);
+                            forward_parent
+                        } else {
+                            unreachable!();
+                        };
+                    }
+
+                    for kind in kinds.into_iter().rev() {
+                        self.builder.start_node(OcrLang::kind_to_raw(kind));
+                    }
+                }
+                Event::AddToken => self.token(),
+                Event::FinishNode => self.builder.finish_node(),
+                Event::Error(e) => self.errors.push(e),
+                Event::Placeholder => {}
+            }
+
+            self.skip_trivia();
+        }
+
+        Parse {
+            green_node: self.builder.finish(),
+            errors: self.errors,
+        }
+    }
+
+    fn token(&mut self) {
+        let Token { kind, text, .. } = self.tokens[self.cursor];
+
+        self.builder.token(OcrLang::kind_to_raw(kind.into()), text);
+
+        self.cursor += 1;
+    }
+
+    fn skip_trivia(&mut self) {
+        while let Some(token) = self.tokens.get(self.cursor) {
+            if !token.kind.is_trivia() {
+                break;
+            }
+
+            self.token()
+        }
     }
 }
