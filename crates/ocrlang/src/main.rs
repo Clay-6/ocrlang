0250d120cdc322594f8e50b3f1e88da61aafe62d
Clay <clay@clay66.dev>
2023-09-22 20:22:14 +0100

    Write simple dummy CLI

---
+++ crates/ocrlang/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    let mut stdout = io::stdout();
+    let stdin = io::stdin();
+
+    loop {
+        write!(stdout, "> ")?;
+        stdout.flush()?;
+        stdin.read_line(&mut input)?;
+
+        let parse = parser::parse(&input);
+        writeln!(stdout, "Parse tree:")?;
+        println!("{}", parse.debug_tree());
+
+        let syntax = parse.syntax();
+
+        let root = ast::Root::cast(syntax).unwrap();
+
+        writeln!(stdout, "AST:")?;
+        dbg!(root.stmts().collect::<Vec<_>>());
+
+        input.clear();
+    }
+}
