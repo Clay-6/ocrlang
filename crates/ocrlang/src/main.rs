c275b1818042d33f509c38118d953bccd464db3d
Clay <clayw@clay66.dev>
2023-10-14 15:44:05 +0100

    Use interpreter in REPL

--- crates/ocrlang/src/main.rs
+++ crates/ocrlang/src/main.rs
@@ -1,28 +1,19 @@
 use std::io::{self, Write};
 
+use interpreter::Interpreter;
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
     let mut stdout = io::stdout();
     let stdin = io::stdin();
+    let mut interpreter = Interpreter::new();
 
     loop {
         write!(stdout, "> ")?;
         stdout.flush()?;
         stdin.read_line(&mut input)?;
 
-        let parse = parser::parse(&input);
-        writeln!(stdout, "Parse tree:")?;
-        println!("{}", parse.debug_tree());
-
-        let syntax = parse.syntax();
-
-        let root = ast::Root::cast(syntax).unwrap();
-
-        writeln!(stdout, "AST:")?;
-        dbg!(root.stmts().collect::<Vec<_>>());
-
-        writeln!(stdout, "HIR:")?;
-        dbg!(hir::lower(root));
+        interpreter.run(&input).unwrap();
 
         input.clear();
     }
